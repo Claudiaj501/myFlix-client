371c55e49d0c2120d8f59232618d8840818bd4c9
Claudiaj501 <claudiaj.501@outlook.com>
2022-10-26 10:02:36 +0200

    adding redux to app

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,29 @@ import ReactDOM from 'react-dom';
 import  MainView  from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 // Import statement to indicate that you need to bundle
 import './index.scss';
+
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      
-        <MainView />
-      
+    return(
+     <Provider store={store}>
+        {/* <Container> */}
+          <MainView />
+        {/* </Container> */}
+      </Provider>
     );
   }
 }
+
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 // Tells React to render your app in the root DOM element
